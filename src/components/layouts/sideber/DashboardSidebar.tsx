e9390bbff2ab662b7a41cb1b2895291419044d53
Khant Kyaw <khantkankei@gmail.com>
2024-07-17 20:51:57 +0630

    agents dummy list table started

--- src/components/layouts/sideber/DashboardSidebar.tsx
+++ src/components/layouts/sideber/DashboardSidebar.tsx
@@ -1,5 +1,5 @@
 import { Layout, Menu, Typography } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { UserOutlined, UserSwitchOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 
@@ -7,53 +7,59 @@ const { Sider } = Layout;
 const { Title } = Typography;
 
 export interface collapseProp {
-  collapsed: boolean;
-  setCollapsed: (value: boolean) => void;
+	collapsed: boolean;
+	setCollapsed: (value: boolean) => void;
 }
 
 const DashboardSidebar = ({ collapsed }: collapseProp) => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleItemClick = (key: string) => {
-    navigate(key);
-  };
+	const handleItemClick = (key: string) => {
+		navigate(key);
+	};
 
-  const navItems = [
-    {
-      key: "users",
-      icon: <UserOutlined />,
-      label: "User",
-    },
-  ];
+	const navItems = [
+		{
+			key: "users",
+			icon: <UserOutlined />,
+			label: "User",
+		},
+		{
+			key: "agents",
+			icon: <UserSwitchOutlined />,
+			label: "Agent",
+		},
+	];
 
-  return (
-    <Sider
-      trigger={null}
-      collapsible
-      collapsed={collapsed}
-      style={{
-        overflow: "auto",
-        height: "100vh",
-        position: "fixed",
-        left: 0,
-        top: 0,
-        bottom: 0,
-        backgroundColor: "#fdfdfd",
-      }}>
-      <div className={`logo-container ${collapsed ? "collapsed" : ""}`}>
-        <Title level={5} className="sidebar-title">
-          Real Estate
-        </Title>
-      </div>
-      <Menu
-        mode="inline"
-        defaultSelectedKeys={["1"]}
-        onClick={({ key }) => handleItemClick(key)}
-        items={navItems}
-        className="custom-menu"
-      />
-    </Sider>
-  );
+	return (
+		<Sider
+			trigger={null}
+			collapsible
+			collapsed={collapsed}
+			style={{
+				overflow: "auto",
+				height: "100vh",
+				position: "fixed",
+				left: 0,
+				top: 0,
+				bottom: 0,
+				backgroundColor: "#fdfdfd",
+			}}
+		>
+			<div className={`logo-container ${collapsed ? "collapsed" : ""}`}>
+				<Title level={5} className='sidebar-title'>
+					Real Estate
+				</Title>
+			</div>
+			<Menu
+				mode='inline'
+				defaultSelectedKeys={["1"]}
+				onClick={({ key }) => handleItemClick(key)}
+				items={navItems}
+				className='custom-menu'
+			/>
+		</Sider>
+	);
 };
 
 export default DashboardSidebar;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,21 +1,26 @@
 import DashboardLayout from "../components/layouts/DashboardLayout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import UserList from "../components/users/UserList";
+import AgentList from "../components/agents/AgentList";
 
 const Router = () => {
-  const config = createBrowserRouter([
-    {
-      path: "/",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/users",
-          element: <UserList />,
-        },
-      ],
-    },
-  ]);
-  return <RouterProvider router={config} />;
+	const config = createBrowserRouter([
+		{
+			path: "/",
+			element: <DashboardLayout />,
+			children: [
+				{
+					path: "/users",
+					element: <UserList />,
+				},
+				{
+					path: "/agents",
+					element: <AgentList />,
+				},
+			],
+		},
+	]);
+	return <RouterProvider router={config} />;
 };
 
 export default Router;
