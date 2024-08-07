9ae1e2c5d2be944e73949856873fe42ff3f77d4c
Khant Kyaw <khantkankei@gmail.com>
2024-08-07 11:51:49 +0630

    route name conflict fixed

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,45 +2,45 @@ import DashboardLayout from "../admin/layouts/DashboardLayout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ClientList from "../admin/components/clients/ClientList";
 import AgentList from "../admin/components/agents/AgentList";
-import TransactionsList from "../admin/components/transactions/TransactionList";
+import TransactionsList from "../admin/components/transactions/TransactionsList";
 import Review from "../admin/components/All Review/Review";
 import Appointments from "../admin/components/appointments/Appointments";
 import PropertyList from "../admin/components/properties/PropertyList";
 
 const Router = () => {
-  const config = createBrowserRouter([
-    {
-      path: "/",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/clients",
-          element: <ClientList />,
-        },
-        {
-          path: "/agents",
-          element: <AgentList />,
-        },
-        {
-          path: "/review",
-          element: <Review />,
-        },
-        {
-          path: "/transactions",
-          element: <TransactionsList />,
-        },
-        {
-          path: "/appointments",
-          element: <Appointments />,
-        },
-        {
-          path: "/properties",
-          element: <PropertyList/>
-        }
-      ],
-    },
-  ]);
-  return <RouterProvider router={config} />;
+	const config = createBrowserRouter([
+		{
+			path: "/",
+			element: <DashboardLayout />,
+			children: [
+				{
+					path: "/clients",
+					element: <ClientList />,
+				},
+				{
+					path: "/agents",
+					element: <AgentList />,
+				},
+				{
+					path: "/review",
+					element: <Review />,
+				},
+				{
+					path: "/transactions",
+					element: <TransactionsList />,
+				},
+				{
+					path: "/appointments",
+					element: <Appointments />,
+				},
+				{
+					path: "/properties",
+					element: <PropertyList />,
+				},
+			],
+		},
+	]);
+	return <RouterProvider router={config} />;
 };
 
 export default Router;
