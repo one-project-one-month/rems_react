7f69616e0cf1d5ca496736c45213e5d43716bacf
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-08-18 01:57:34 +0630

    fix: code changes

--- src/type/type.ts
+++ src/type/type.ts
@@ -1,79 +1,81 @@
 export interface Review {
-    reviewId: number;
-    userId: number;
-    propertyId: number;
-    rating: number;
-    comments: string;
-    dateCreated: Date;
-  }
+  reviewId: number;
+  userId: number;
+  propertyId: number;
+  rating: number;
+  comments: string;
+  dateCreated: Date;
+}
 
 export interface Client {
-    firstName: string,
-    lastName: string,
-    phone: string,
-    email: string,
-    address: string,
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  address: string;
 }
 
 export interface Agent {
-    agency_name: string,
-    license_number: string,
-    phone: string,
-    email: string
+  agency_name: string;
+  license_number: string;
+  phone: string;
+  email: string;
 }
 
-export interface Transaction{
-    transactionId: number,
-    transactionDate: Date,
-    salePrice: number,
-    commission: number,
-    status: string,
+export interface Transaction {
+  transactionId: number;
+  transactionDate: Date;
+  salePrice: number;
+  commission: number;
+  status: string;
 }
 
 export interface Transactions {
-    transaction: Transaction,
-    property: Property,
-    client: Client,
+  transaction: Transaction;
+  property: Property;
+  client: Client;
 }
 
 export interface transactionResponse {
-    isSuccess: string,
-    isError: string,
-    data: Object
-    message: string
+  isSuccess: string;
+  isError: string;
+  data: Object;
+  message: string;
 }
+
 interface Images {
-    imageId: number,
-    propertyId: number,
-    imageUrl: string,
-    description: string,
-    dateUploaded: string
+  imageId: number;
+  propertyId: number;
+  imageUrl: string;
+  description: string;
+  dateUploaded: string;
 }
-export interface Properties{
-    property: Property,
-    images: Images[],
-    reviews: Review[]
+
+export interface Properties {
+  property: Property;
+  images: Images[];
+  reviews: Review[];
 }
 
 export interface Property {
-    propertyId: number,
-    agent?: Agent,
-    agentId?: number,
-    address: string,
-    city: string,
-    state: string,
-    zipCode: string,
-    propertyType: string,
-    price: number,
-    size: number,
-    numberOfBedrooms: number,
-    numberOfBathrooms: number,
-    yearBuilt: number,
-    description: string,
-    status: string,
-    availiablityType: string,
-    minrentalPeriod: number,
-    approvedby: string,
-    adddate: Date,
-    editdate: Date
+  propertyId: number;
+  agent?: Agent;
+  agentId?: number;
+  address: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  propertyType: string;
+  price: number;
+  size: number;
+  numberOfBedrooms: number;
+  numberOfBathrooms: number;
+  yearBuilt: number;
+  description: string;
+  status: string;
+  availiablityType: string;
+  minrentalPeriod: number;
+  approvedby: string;
+  adddate: Date;
+  editdate: Date;
 }
