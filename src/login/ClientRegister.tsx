ff71fbeddfc05ba08bd2300e4768e7eaee1731bc
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-09-19 01:01:35 +0700

    Fixed agent registration page and fixed code

--- src/login/ClientRegister.tsx
+++ src/login/ClientRegister.tsx
@@ -4,16 +4,15 @@ import axios from "axios";
 import { Button, Form, Input, Typography } from "antd";
 import { toast } from "sonner";
 
-const ClientRegister: React.FC = () => {
+const ClientRegister = () => {
   const navigate = useNavigate();
 
-  const [error, setError] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-  const [success, setSuccess] = useState<string>("");
+  const [loading, setLoading] = useState(false);
 
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   const { Link, Text } = Typography;
+
   const handleSubmit = async (values: {
     address: string;
     confirmPassword: string;
@@ -23,71 +22,30 @@ const ClientRegister: React.FC = () => {
     password: string;
     phone: string;
   }) => {
-    console.log("Values", values);
-    const {
-      address,
-      confirmPassword,
-      email,
-      firstName,
-      lastName,
-      password,
-      phone,
-    } = values;
-
-    // Clear previous messages
-    setError("");
-    setSuccess("");
-
-    // Validate all fields
-    if (
-      !firstName ||
-      !lastName ||
-      !email ||
-      !password ||
-      !confirmPassword ||
-      !phone ||
-      !address
-    ) {
-      setError("Please fill in all fields.");
-      toast.error(error);
-      return;
-    }
-
-    // Validate email format
-    if (!emailRegex.test(email)) {
-      setError("Please enter a valid email address.");
-      toast.error(error);
-      return;
-    }
-
-    // Validate password match
-    if (password !== confirmPassword) {
-      setError("Passwords do not match.");
-      toast.error(error);
-      return;
-    }
+    const { address, email, firstName, lastName, password, phone } = values;
 
     try {
       // Simulate an API call
       setLoading(true);
-      await axios.post("http://65.18.112.78:44010/rems/api/v1/clients", {
-        firstName,
-        lastName,
-        email,
-        password,
-        phone,
-        address,
-      });
-
-      setSuccess("Successfully signed up!");
-      toast.success(success);
-      setError("");
+      const res = await axios.post(
+        "http://65.18.112.78:44010/rems/api/v1/clients",
+        {
+          firstName,
+          lastName,
+          email,
+          password,
+          phone,
+          address,
+        }
+      );
+      toast.success(res.data.message);
 
       // Redirect after a successful registration
       setTimeout(() => navigate("/"), 2000);
-    } catch (err) {
-      setError("Registration failed. Please try again.");
-      toast.error(error);
+    } catch (error) {
+      toast.error(
+        (error as any)?.response?.data?.message || "An error occurred."
+      );
     } finally {
       setLoading(false);
     }
@@ -107,28 +65,10 @@ const ClientRegister: React.FC = () => {
         </Text>
         {/* First Name and Last Name */}
         <div className="flex flex-wrap gap-2">
-          {/* <input
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              className="bg-gray-200 focus:outline-none flex-1 border-b-2 p-2 rounded-md font-bold placeholder:font-normal"
-              type="text"
-              placeholder="First Name"
-              required
-              font-bold
-            />
-
-            <input
-              className="bg-gray-200 focus:outline-none flex-1 border-b-2 p-2 rounded-md font-bold placeholder:font-normal"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              type="text"
-              placeholder="Last Name"
-              required
-            /> */}
           <Form.Item
             name="firstName"
             rules={[
-              { required: true, message: "Please input your First Name!" },
+              { required: true, message: "Please enter your First Name!" },
             ]}>
             <Input placeholder="First Name" size="large" />
           </Form.Item>
@@ -136,36 +76,27 @@ const ClientRegister: React.FC = () => {
           <Form.Item
             name="lastName"
             rules={[
-              { required: true, message: "Please input your Last Name!" },
+              { required: true, message: "Please enter your Last Name!" },
             ]}>
             <Input placeholder="Last Name" size="large" />
           </Form.Item>
         </div>
 
-        {/* <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-gray-200 focus:outline-none border-b-2 p-2 rounded-md font-bold placeholder:font-normal"
-            placeholder="Email"
-            required
-          /> */}
-
         {/* Email */}
         <Form.Item
           name="email"
           rules={[
-            { required: true, message: "Please input your email!" },
+            { required: true, message: "Please enter your email!" },
             { pattern: emailRegex, message: "Please enter a valid email!" },
           ]}>
-          <Input type="email" placeholder="Email" />
+          <Input type="email" placeholder="Email" size="large" />
         </Form.Item>
 
         {/* Password */}
         <Form.Item
           name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}>
-          <Input.Password placeholder="Password" />
+          rules={[{ required: true, message: "Please enter your password!" }]}>
+          <Input.Password placeholder="Password" size="large" />
         </Form.Item>
 
         {/* Confirm Password */}
@@ -184,78 +115,23 @@ const ClientRegister: React.FC = () => {
               },
             }),
           ]}>
-          <Input.Password placeholder="Confirm Password" />
+          <Input.Password placeholder="Confirm Password" size="large" />
         </Form.Item>
-
-        {/* <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="bg-gray-200 focus:outline-none border-b-2 p-2 rounded-md font-bold placeholder:font-normal"
-            placeholder="Password"
-            required
-          /> */}
-
-        {/* <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            className="bg-gray-200 focus:outline-none border-b-2 p-2 rounded-md font-bold placeholder:font-normal"
-            placeholder="Confirm Password"
-            required
-          /> */}
-
-        {/* <textarea
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            rows={2}
-            cols={10}
-            className="focus:outline-none bg-gray-200 p-3 font-bold placeholder:font-normal"
-            placeholder="Your address..."
-            required
-          /> */}
 
         {/* Phone Number */}
         <Form.Item
           name="phone"
           rules={[
-            { required: true, message: "Please input your phone number!" },
+            { required: true, message: "Please enter your phone number!" },
           ]}>
-          <Input type="tel" placeholder="Phone Number" />
+          <Input type="tel" placeholder="Phone Number" size="large" />
         </Form.Item>
 
         {/* Address */}
-        <Form.Item
-          name="address"
-          rules={[{ required: true, message: "Please input your address!" }]}>
-          <Input.TextArea rows={2} placeholder="Your address..." />
+        <Form.Item name="address">
+          <Input.TextArea rows={2} placeholder="Address" size="large" />
         </Form.Item>
 
-        {/* <input
-            className="bg-gray-200 focus:outline-none border-b-2 p-2 rounded-md font-bold placeholder:font-normal"
-            type="tel"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            placeholder="Phone number"
-            required
-          /> */}
-
-        {/* Error and Success Messages */}
-        {error && (
-          <p className="w-[50%] text-center mt-5 text-red-500">{error}</p>
-        )}
-        {success && (
-          <p className="w-[50%] text-center mt-5 text-green-500">{success}</p>
-        )}
-        {loading && (
-          <p className="w-[50%] text-center mt-5 text-green-500">Loading</p>
-        )}
-
-        {/* <button
-          className="bg-blue-500 mt-10 px-5 py-2 rounded-md text-white font-bold hover:shadow-lg hover:bg-blue-400"
-          type="submit">
-          Sign Up
-        </button> */}
         <Form.Item className="text-center items-center">
           <Button
             type="primary"
--- src/login/Register.tsx
+++ src/login/Register.tsx
@@ -1,36 +1,43 @@
-import { useState } from "react"
-import AgentRegister from "./AgentRegister"
-import ClientRegister from "./ClientRegister"
+import { useState } from "react";
+import AgentRegister from "./AgentRegister";
+import ClientRegister from "./ClientRegister";
+// import { Card, Typography } from "antd";
 
-const Register:React.FC = () => {
+const Register: React.FC = () => {
+  const [role, setRole] = useState<string>("client");
 
-  const [role, setRole] = useState<string>('client');
+  // const { Title } = Typography;
 
   const renderRegister = () => {
-    if (role === "client") return <ClientRegister />
-    if (role === "agent") return <AgentRegister />
-  }
+    if (role === "client") return <ClientRegister />;
+    if (role === "agent") return <AgentRegister />;
+  };
 
   return (
-    <div className="p-5">    
+    <div className="p-5">
       <div className="flex flex-col flex-wrap gap-5 justify-center items-center mt-5">
         <p className="text-[1.2rem]">Please choose the role</p>
         <div className="flex gap-2 justify-center items-center">
           <p className="font-bold">Register as</p>
-          <select className="border rounded-md bg-white font-bold px-3 py-1" value={role} onChange={(e) => setRole(e.target.value)}>
+          <select
+            className="border rounded-md bg-white font-bold px-3 py-1"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}>
             <option value="client">Client</option>
             <option value="agent">Agent</option>
           </select>
         </div>
       </div>
 
-      <div className="flex justify-center items-center">
-        {
-          renderRegister()
-        }
-      </div>
-    </div>
-  )
-}
+      {/* <Card style={{ width: 300 }}>
+        <Title level={5} className="pb-6 text-gray-600 text-center">
+          Please choose the role
+        </Title>
+      </Card> */}
 
-export default Register
+      <div className="flex justify-center items-center">{renderRegister()}</div>
+    </div>
+  );
+};
+
+export default Register;
