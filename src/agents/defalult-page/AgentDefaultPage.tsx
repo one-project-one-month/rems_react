aea3db181abd5ee4cbc56ecd4af2eb6088a2a94a
Thein Naing <iphonesamsungs77@gmail.com>
2024-09-20 22:09:33 +0630

    a little change in agent nav code

---
+++ src/agents/defalult-page/AgentDefaultPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const AgentDefaultPage = () => {
+  return (
+    <div className='no-scrollbar'>
+      <div className='background-pic flex justify-center items-center font-bold text-2xl bg-red-500 w-[100%] h-[500px]'>
+        Vinyl Post
+      </div>
+      <div className='carousel-with-yangoncity flex justify-center items-center font-bold text-2xl bg-gray-300 h-[200px]'> 
+        Card For Yangon Recommended
+      </div>
+      <div className='carousel-with-yangoncity flex justify-center items-center font-bold text-2xl bg-green-300 h-[200px]'> 
+        3 Cards
+      </div>
+      <div className='carousel-with-yangoncity flex justify-center items-center font-bold text-2xl bg-gray-500 h-[200px]'> 
+        All Cards
+      </div>
+      <div className='carousel-with-yangoncity flex justify-center items-center font-bold text-2xl bg-teal-800 h-[200px]'> 
+        Footer
+      </div>
+    </div>
+  )
+}
+
+export default AgentDefaultPage
--- src/client/layouts/Navbar.tsx
+++ src/client/layouts/Navbar.tsx
@@ -8,8 +8,6 @@ const Navbar: React.FC = () => {
 
 	return (
 		<div>
-
-
 			<nav className='bg-blue-800 fixed top-0 left-0 w-full z-50  shadow-md'>
 				<div className='container mx-auto flex justify-between items-center p-4'>
 					<div className='text-white text-xl font-bold'>REMS</div>
